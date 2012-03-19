a6c976edfd17167a19071d491e95d0f54548ef95
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-03-19 16:32:28 +0000

    GPLv2 headers for WindowsMonitor; reviewed by epowell
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.WindowsMonitor@55479 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/WindowsMonitor/PerfRpc.py
+++ ZenPacks/zenoss/WindowsMonitor/PerfRpc.py
@@ -1,8 +1,15 @@
-##########################################################################
+###########################################################################
 #
-#   Copyright 2009 Zenoss, Inc. All Rights Reserved.
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2009, Zenoss Inc.
 #
-##########################################################################
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 or (at your
+# option) any later version as published by the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
 
 from pysamba.rpc.Rpc import Rpc
 from pysamba.talloc import talloc_zero, talloc_array, talloc_free
--- ZenPacks/zenoss/WindowsMonitor/services/WinPerfConfig.py
+++ ZenPacks/zenoss/WindowsMonitor/services/WinPerfConfig.py
@@ -1,9 +1,15 @@
 ###########################################################################
 #
-# Copyright 2007-2009 Zenoss, Inc.  All Rights Reserved.
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2007-2009, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 or (at your
+# option) any later version as published by the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
 #
 ###########################################################################
-
 __doc__='''WinPerfConfig
 
 ZenHub service for handling zenwinperf configuration
