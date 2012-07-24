12fb02ee6144e8075cf18135844b58ac3abd3fbe
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-24 05:16:47 +0000

    [trunk/core] update source headers per legal for 4.2.x and beyond using Summer's script fu; reviewed by gstreiff jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.WindowsMonitor@61801 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/WindowsMonitor/Constants.py
+++ ZenPacks/zenoss/WindowsMonitor/Constants.py
@@ -1,15 +1,12 @@
-###########################################################################
-#
-# This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2007, Zenoss Inc.
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 or (at your
-# option) any later version as published by the Free Software Foundation.
-#
-# For complete information please visit: http://www.zenoss.com/oss/
-#
-###########################################################################
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2007, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
+
 
 TIMEOUT_CODE = 2147209215
 RPC_ERROR_CODE = 2147023170
@@ -3634,4 +3631,3 @@ ERROR_CODE_MAP = {'0': ('ERROR_SUCCESS', 'The operation completed successfully.'
  '997': ('ERROR_IO_PENDING', 'Overlapped I/O operation is in progress.'),
  '998': ('ERROR_NOACCESS', 'Invalid access to memory location.'),
  '999': ('ERROR_SWAPERROR', 'Error performing inpage operation.')}
-
