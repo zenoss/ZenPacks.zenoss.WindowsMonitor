7d3d00fce4cbe0f95ca8b956f99001ce04eec256
cluther <cluther@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2013-06-26 16:05:09 +0000

    Remove Zenoss from INSTALL_REQUIRES.
    This is to allow trunk ZenPacks to be tested on current stable Zenoss
    versions.
    
    Reviewed by gstreiff and jcausey.
    
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.WindowsMonitor@74543 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = []
-INSTALL_REQUIRES = ['ZenPacks.zenoss.PySamba', 'Zenoss >= 4.1.70']
+INSTALL_REQUIRES = ['ZenPacks.zenoss.PySamba']
 COMPAT_ZENOSS_VERS = '>=4.1.70'
 PREV_ZENPACK_NAME = 'ZenPacks.zenoss.WindowsMonitor'
 
