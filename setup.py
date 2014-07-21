b15658ed5f44eb745364be45e4cb390495d0d6a6
Chet Luther <chet.luther@gmail.com>
2014-07-21 09:19:23 -0500

    Version: 1.1.0 -> 1.1.1dev.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = 'ZenPacks.zenoss.WindowsMonitor'
-VERSION = '1.1.0'
+VERSION = '1.1.1dev'
 AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
